81160e90ba83e04ef97b3d39b6f9d171d2b69bea
Emilio J. Gomez <moleculax@gmail.com>
2025-12-19 08:50:39 -0300

    Actualizacion

---
+++ lib/screens/Logueo.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../widgets/login_form.dart'; // ← importa tu función LoginForm
+
+// class LogueoPage extends StatelessWidget {
+//   const LogueoPage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text("Logueo"),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: LoginForm(), // Hago llamado al formulario Login
+//       ),
+//     );
+//   }
+// }
